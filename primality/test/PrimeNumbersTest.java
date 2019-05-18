76a0c8ec3d2e7c20fc9e8a3f62496b9f3b0f2ac8
Michael Krause <michael.krause@ucsc.edu>
2019-05-18 13:21:22 -0700

    Restructured repo

---
+++ primality/test/PrimeNumbersTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import java.util.InputMismatchException;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PrimeNumbersTest {
+
+    @Test
+    public void TestPrimeNumber_true() {
+        boolean pred = PrimeNumbers.isPrime(3);
+        assertTrue(pred);
+    }
+
+    @Test
+    public void TestPrimeNumber_false() {
+        boolean pred = PrimeNumbers.isPrime(4);
+        assertFalse(pred);
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void TestPrimeNumber_wrong_input() {
+        PrimeNumbers.isPrime(-3);
+    }
+
+}
